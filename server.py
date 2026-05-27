d6cf193bb96007b65351f4fafe44d9632fdefd24
Srinivas Palisetti <palisettis77@gmail.com>
2026-05-27 15:06:57 -0500

    Add server.py and requirements

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_from_directory
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:filename>')
+def files(filename):
+    return send_from_directory('.', filename)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
