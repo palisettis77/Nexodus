d47815992cba9beefdcfeed1957b9f0d15fb0e3a
Srinivas Palisetti <palisettis77@gmail.com>
2026-05-27 15:20:04 -0500

    Fix server path

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
+import os
 from flask import Flask, send_from_directory
+
 app = Flask(__name__)
+
+BASE = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def home():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory(BASE, 'index.html')
 
 @app.route('/<path:filename>')
 def files(filename):
-    return send_from_directory('.', filename)
+    return send_from_directory(BASE, filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
